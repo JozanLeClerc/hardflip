a9993e59e2a1514654abe3c04c89c22fa339c1af
Joe <rbo@gmx.us>
2024-01-18 20:20:20 +0100

    go

--- c_hardflip.go
+++ c_hardflip.go
@@ -63,7 +63,7 @@ type HardData struct {
 
 func main() {
 	data_dir := c_get_data_dir()
-	opts := HardOpts{true, true, false, "$TERMINAL"}
+	opts := HardOpts{true, true, false, ""}
 	ldirs := c_load_data_dir(data_dir, opts)
 	litems := c_load_litems(ldirs)
 	data := HardData{
